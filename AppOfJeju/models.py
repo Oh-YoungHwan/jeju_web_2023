ec61e56f12bc5d26ab8631d47894019cd5a97db5
Yu JeongMin <polar522@naver.com>
2023-11-20 19:01:08 +0900

    11_20_yjm
    commit

--- AppOfJeju/models.py
+++ AppOfJeju/models.py
@@ -9,3 +9,19 @@ class Jeju(models.Model):
 
     def __str__(self):
         return self.title
+
+class CropMarketData(models.Model):
+    crop_type = models.CharField(max_length=20)
+    supplier = models.CharField(max_length=20, blank=True)
+    origin = models.CharField(max_length=20, blank=True)
+    crop_date = models.DateField(null=False)
+    crop_supply = models.IntegerField(null=True)
+    crop_price = models.IntegerField(null=True)
+
+class PredictionData(models.Model):
+    crop_type = models.CharField(max_length=20)
+    supplier = models.CharField(max_length=20, blank=True)
+    origin = models.CharField(max_length=20, blank=True)
+    crop_date = models.DateField(null=False)
+    crop_price = models.FloatField(null=False)
+    ai_model = models.CharField(max_length=30, null=False, blank=False)
